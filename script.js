9fc81c7e0a9929a5edd678eb93dba725f7f42705
Naomi Matos-Lopez <188728185+shybit0@users.noreply.github.com>
2025-02-17 19:47:54 -0600

    Add functionality for displaying current date

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secsEl = document.getElementById("secs");
 
-let taskDeadline = "18 Feb 2025";
+let taskDeadline = "18 Feb 2026";
+const dateEl = document.querySelector(".date");
+dateEl.innerHTML = getCurrentDate();
 
 function countdown(){
     const deadline = new Date(taskDeadline);
@@ -20,12 +22,68 @@ function countdown(){
     hoursEl.innerHTML = formatTime(hours);
     minsEl.innerHTML = formatTime(minutes);
     secsEl.innerHTML = formatTime(seconds);
-
-    console.log(days, hours, minutes, seconds);
 }
 
 function formatTime(time){
     return time < 10 ? (`0${time}`) : time;
+}
+
+function getCurrentDate(){
+    let currentDate = new Date();
+    let weekDay = getDayName(currentDate.getDay());
+    let day = currentDate.getDate();
+    let month = getMonthName(currentDate.getMonth());
+    let year = currentDate.getFullYear();
+
+    return `${weekDay}, ${day} ${month} ${year}`;
+}
+
+function getDayName(num){
+    switch(num){
+        case 0: 
+            return 'Sunday';
+        case 1: 
+            return 'Monday';
+        case 2: 
+            return 'Tuesday';    
+        case 3: 
+            return 'Wednesday';
+        case 4: 
+            return 'Thursday';
+        case 5: 
+            return 'Friday';
+        case 6: 
+            return 'Saturday';
+    }
+}
+
+function getMonthName(num){
+    switch(num){
+        case 0: 
+            return 'January';
+        case 1: 
+            return 'February';
+        case 2: 
+            return 'March';    
+        case 3: 
+            return 'April';
+        case 4: 
+            return 'May';
+        case 5: 
+            return 'June';
+        case 6: 
+            return 'July';
+        case 7: 
+            return 'August';
+        case 8: 
+            return 'September';
+        case 9: 
+            return 'October';
+        case 10: 
+            return 'November';
+        case 11: 
+            return 'December';
+    }
 }
 
 countdown();
